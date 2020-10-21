ff51a7051d28ff36ec22b8913b6df66642621717
胡枫 <hufeng@lesteels.com>
2020-10-21 14:58:42 +0800

    优化合计

--- src/maple.vue
+++ src/maple.vue
@@ -556,7 +556,7 @@ export default {
                         ...extraItem
                     };
                     // 根据callback返回的notAddabled字段，判断是否添加数据
-                    if (!o.notAddabled && o.mapleTotal !== "合计") {
+                    if (!o.notAddabled && !(o.mapleTotal === "合计" && i === d.length - 1)) {
                         data.push({
                             ...o,
                             notAddabled: undefined,
